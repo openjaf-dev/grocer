608e25562aa05bbd21e370e5b8341cede37d1173
Miguel Sancho <sanchocuba@gmail.com>
2014-09-27 13:08:11 -0400

    udpate

--- lib/dashboard/by_time.rb
+++ lib/dashboard/by_time.rb
@@ -12,12 +12,14 @@ module Dashboard
       end
 
       def data_by(collection, fun = 'time_line', date_field = 'created_at', opts = {})
-        opts[:filter] ||= 'weeks'
+        opts[:filter] ||= 'week'
         collection = collect_by(collection, fun, date_field, opts ).sort
         [{:data => collection }]
       end
 
       def collect_by(collection, fun, date_field, opts = {})
+        puts "************ fun collect_by #{fun}"
+        
         collection = collection.group_by {|o| opts[:filter].present? ? o.send(date_field).send("beginning_of_#{opts[:filter]}") : o.send(date_field)}
         fun == 'time_line' ? collection.map {|k, v| [k,cal(v)] } : collection.sort.map {|c| [self.send("beginning_of_#{opts[:filter]}")[c[0]], cal(c[1])]} 
       end
