0d2d3a95a16bb3a65ff966c63d0091e7ca69bf10
Miguel Sancho <sanchocuba@gmail.com>
2014-09-29 09:41:04 -0400

    remove line that convert v to a activerecort relation

--- lib/dashboard/by_time.rb
+++ lib/dashboard/by_time.rb
@@ -9,17 +9,12 @@ module Dashboard
     module ClassMethods
 
       def data_by(collection, base_cal, fun, date_field, calculation, opts = {})
-        puts "**************** colection.class #{collection.class}"
-        
-        puts "************ fun collect_by #{fun}"
-        
         collection = collection.group_by {|o| opts[:filter].present? ? o.send(date_field).send("beginning_of_#{opts[:filter]}") : o.send('placed_on')}
        
         p = eval( "lambda { |coll| coll.map#{self.def_meth(base_cal)}.#{calculation} }")
 
-        
         collection = if fun =='time_line'
-          collection.map { |k, v| [ k, p.call( self.where(id: v.map(&:id)) ) ] } 
+          collection.map { |k, v| [ k, p.call(v) ] } 
         else
           collection.sort.map {|c| [send("beginning_of_#{opts[:filter]}")[c[0]], [ k, p.call(c[1]) ] ]}
         end
