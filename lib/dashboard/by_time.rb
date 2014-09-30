12bc1e28e4fce66393f6f14e5aa74cb7a4ab5e34
Miguel Sancho <sanchocuba@gmail.com>
2014-09-30 09:32:28 -0400

    fix 1-d graphics

--- lib/dashboard/by_time.rb
+++ lib/dashboard/by_time.rb
@@ -10,7 +10,7 @@ module Dashboard
 
       def data_by(collection, base_cal, fun, date_field, calculation, acumulate = nil )
         
-        acumulate = "beginning_of_#{acumulate}" if acumulate.present? and fun =='2-d'
+        acumulate = "beginning_of_#{acumulate}" if acumulate.present? && fun =='2-d'
         
         collection = collection.group_by do |o| 
           acumulate.present? ? o.send(date_field).send(acumulate) : o.send(date_field)
@@ -21,7 +21,7 @@ module Dashboard
         collection = if fun =='2-d' 
           collection.map {|k,v| [k,p.call(v)]} 
         else 
-          collection.sort.map {|c| [ acumulate[c[0]], p.call(c[1]) ] }
+          collection.sort.map {|c| [ send(acumulate)[c[0]], p.call(c[1]) ] }
         end  
         
         [{:data => collection}]
